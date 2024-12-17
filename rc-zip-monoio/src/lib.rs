d1ccb08a985d287733cd1c12113c00bd6ecb6ad1
Amos Wenger <amos@bearcove.net>
2024-12-17 23:39:15 +0100

    Try out monoio

---
+++ rc-zip-monoio/src/lib.rs
@@ -0,0 +1,42 @@
+//! A library for reading zip files asynchronously using monoio I/O traits,
+//! based on top of [rc-zip](https://crates.io/crates/rc-zip).
+//!
+//! See also:
+//!
+//!   * [rc-zip-sync](https://crates.io/crates/rc-zip-sync) for using std I/O traits
+//!   * [rc-zip-tokio](https://crates.io/crates/rc-zip-tokio) for using tokio traits
+
+use monoio::{buf::IoBufMut, fs::File};
+use rc_zip::{
+    error::Error,
+    fsm::{ArchiveFsm, FsmResult},
+    parse::Archive,
+};
+
+pub async fn read_zip_from_file(file: &File) -> Result<Archive, Error> {
+    let meta = file.metadata().await?;
+
+    let mut buf = vec![0u8; 128 * 1024];
+    let mut fsm = ArchiveFsm::new(meta.len());
+    loop {
+        if let Some(offset) = fsm.wants_read() {
+            let dst = fsm.space();
+            let max_read = dst.len();
+            let slice = IoBufMut::slice_mut(buf, 0..max_read);
+            let (res, slice) = file.read_at(slice, offset).await;
+            let n = res?;
+
+            let src = &slice[..n];
+            let dst = &mut dst[..n];
+            dst.copy_from_slice(src);
+
+            fsm.fill(n);
+            buf = slice.into_inner();
+        }
+
+        fsm = match fsm.process()? {
+            FsmResult::Done(archive) => break Ok(archive),
+            FsmResult::Continue(fsm) => fsm,
+        }
+    }
+}

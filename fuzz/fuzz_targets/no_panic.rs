e4af17413c78b95c53611aa29c082debc1dedb79
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-10-29 20:05:55 -0600

    fuzz: include basic entry operations

--- fuzz/fuzz_targets/no_panic.rs
+++ fuzz/fuzz_targets/no_panic.rs
@@ -3,5 +3,11 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = rc_zip_sync::ReadZip::read_zip(&data);
+    let Ok(archive) = rc_zip_sync::ReadZip::read_zip(&data) else {
+        return;
+    };
+    for entry in archive.entries() {
+        let _ = entry.sanitized_name();
+        let _ = entry.reader();
+    }
 });
